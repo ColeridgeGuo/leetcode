13acd3b2d7ac287f4592723c9b14de05526d5514
Yingxuan Guo <yingxuan.guo9771@gmail.com>
2021-01-19 22:22:39 -0800

    LC_41 description

--- LC_41/First Missing Positive.py
+++ LC_41/First Missing Positive.py
@@ -1,3 +1,9 @@
+"""
+Given an unsorted integer array nums, find the smallest missing positive integer
+
+Follow up: Could you implement an algorithm that runs in O(n) time and uses
+constant extra space.?
+"""
 from typing import List
 from common_funcs import stringToList
 
