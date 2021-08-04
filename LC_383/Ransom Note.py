9a3178d82ed9d8046fc9ed3147432e37138ae3f3
Yingxuan Guo <yingxuan.guo9771@gmail.com>
2021-08-04 04:14:52 -0700

    LC_383 modified

--- LC_383/Ransom Note.py
+++ LC_383/Ransom Note.py
@@ -10,10 +10,19 @@ from common_funcs import stringToString
 
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        """
+        Substraction between Counters keeps only positive counts, so if some 
+        chars in note are not matched in magazine, their difference would not be 
+        empty so return False; otherwise all chars in note are matched so True
+        """
         from collections import Counter
         return not Counter(ransomNote) - Counter(magazine)
-    
+
     def canConstruct_dictionary(self, ransomNote: str, magazine: str) -> bool:
+        """
+        Construct a Counter using chars in magazine and match each char in note 
+        to one in magazine, return False if one char in note fails to match
+        """
         from collections import defaultdict
         counter = defaultdict(int)
         for char in magazine:
@@ -27,6 +36,19 @@ class Solution:
                 counter[char] -= 1
         return True
 
+    def canConstruct_dictionary_2(self, ransomNote: str, magazine: str) -> bool:
+        """
+        Same as the above method but more concise
+        """
+        counter = {}
+        for char in magazine:
+            counter[char] = counter.get(char, 0) + 1
+        for char in ransomNote:
+            if counter.get(char, 0) <= 0:
+                return False
+            counter[char] -= 1
+        return True
+
 
 def main():
     while True:
@@ -35,11 +57,11 @@ def main():
             ransomNote = stringToString(line)
             line = input()
             magazine = stringToString(line)
-            
+
             sol = Solution()
             ret_cnt = sol.canConstruct(ransomNote, magazine)
             ret_dic = sol.canConstruct_dictionary(ransomNote, magazine)
-            
+
             print(f"Solved using Python's Counter: {ret_cnt}")
             print(f"Solved using dictionary:       {ret_dic}")
         except StopIteration:
