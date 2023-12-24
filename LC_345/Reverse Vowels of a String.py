6c28c9a6e2cfe75f7566eedec44c0825fb4fb75b
Yingxuan Guo <yingxuan.guo9771@gmail.com>
2023-12-23 22:42:44 -0800

    modified LC_345

--- LC_345/Reverse Vowels of a String.py
+++ LC_345/Reverse Vowels of a String.py
@@ -1,6 +1,8 @@
 """
-Write a function that takes a string as input and reverse only the vowels of a
-string.
+Given a string s, reverse only all the vowels in the string and return it.
+
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower
+and upper cases, more than once.
 """
 from common_funcs import stringToString
 
@@ -21,16 +23,37 @@ class Solution:
                 r -= 1
         return ''.join(chars)
 
+    def reverseVowels_2(self, s: str) -> str:
+        vowels = set("aeiouAEIOU")
+        ans = []
+        s_vowels = []
+        for i in range(len(s)):
+            if s[i] in vowels:
+                s_vowels.append(s[i])
+                ans.append("")
+            else:
+                ans.append(s[i])
+        j = -1
+        for i in range(len(ans)):
+            if ans[i] == "":
+                ans[i] = s_vowels[j]
+                j -= 1
+        return ''.join(ans)
+
 
 def main():
     while True:
         try:
             line = input()
             s = stringToString(line)
+
+            sol = Solution()
+
+            ret = sol.reverseVowels(s)
+            ret2 = sol.reverseVowels_2(s)
             
-            ret = Solution().reverseVowels(s)
-            
-            print(ret)
+            print(f"Solved by moving pointers:         {ret}")
+            print(f"Solved by constructing new string: {ret2}")
         except StopIteration:
             break
 
