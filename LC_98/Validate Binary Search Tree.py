e3de73d80ee3397581beec195edf4849276bc744
Yingxuan Guo <yingxuan.guo9771@gmail.com>
2023-12-23 22:09:31 -0800

    modified LC_98

--- LC_98/Validate Binary Search Tree.py
+++ LC_98/Validate Binary Search Tree.py
@@ -1,4 +1,11 @@
 """
+Given the root of a binary tree, determine if it is a valid binary search tree.
+
+A valid BST is defined as follows:
+
+- The left subtree of a node contains only keys less than the node's key.
+- The right subtree of a node contains only keys greater than the node's key.
+- Both the left and right subtrees must also be binary search trees.
 """
 from typing import Optional
 from common_funcs import TreeNode, stringToTreeNode
