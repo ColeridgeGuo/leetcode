ea637f96527e96054e5bcfb808dc7325eaee9069
Yingxuan Guo <yingxuan.guo9771@gmail.com>
2023-12-31 21:31:54 -0800

    modified LC_238

--- LC_238/Product of Array Except Self.py
+++ LC_238/Product of Array Except Self.py
@@ -19,45 +19,48 @@ class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         """
         Construct two arrays: the products of all the numbers to the left and
-        to the right of each number. Multiply the products at each position to
-        achieve "product but self".
+        to the right of each number, i.e., prefix product and suffix product.
+        Multiply the products at each position to achieve "product but self".
         Time Complexity: O(n)
         Space Complexity: O(n)
         """
-        L, R, res = [0] * len(nums), [0] * len(nums), [0] * len(nums)
-        L[0], R[-1] = 1, 1
+        prefix = [0] * len(nums)
+        suffix = [0] * len(nums)
+        prefix[0] = 1
+        suffix[-1] = 1
         for i in range(1, len(nums)):
-            L[i] = nums[i - 1] * L[i - 1]
-        for i in range(len(nums) - 2, -1, -1):
-            R[i] = nums[i + 1] * R[i + 1]
-        res = [L[i] * R[i] for i in range(len(nums))]
-        return res
-    
+            prefix[i] = nums[i-1] * prefix[i-1]
+            suffix[~i] = nums[~i + 1] * suffix[~i + 1]
+        return [prefix[i] * suffix[i] for i in range(len(nums))]
+
     def productExceptSelf_2(self, nums: List[int]) -> List[int]:
         """
         Similar to method 1 but without using two extra arrays, but instead
         store the products in the output array directly in two passes.
+        Time Complexity: O(n)
+        Space Complexity: O(1)
         """
         res = [0] * len(nums)
         res[0] = 1
         for i in range(1, len(nums)):
             res[i] = nums[i - 1] * res[i - 1]
-        R = 1
+        suffix_product = 1
         for i in range(len(nums) - 1, -1, -1):
-            res[i] *= R
-            R *= nums[i]
+            res[i] *= suffix_product
+            suffix_product *= nums[i]
         return res
+
 
 def main():
     while True:
         try:
             line = input()
             nums = stringToList(line)
-            
+
             sol = Solution()
             ret = sol.productExceptSelf(nums)
             ret2 = sol.productExceptSelf_2(nums)
-            
+
             out = listToString(ret)
             out2 = listToString(ret2)
             print(out)
