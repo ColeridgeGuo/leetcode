3ca663d8909c10549195422e23dc21fe674e8f98
Yingxuan Guo <yingxuan.guo9771@gmail.com>
2024-01-03 00:44:16 -0800

    modified LC_443

--- LC_443/String Compression.py
+++ LC_443/String Compression.py
@@ -10,32 +10,8 @@ from common_funcs import stringToList
 
 
 class Solution:
-    def compress(self, chars: List[str]) -> int:
-        """
-            Time Complexity: O(n)
-            Space Complexity: O(n)
-        """
-        # pointer, number of current chars, current character
-        pt, count, curr_char = 1, 1, chars[0]
-        counter = []  # a list to keep the compressed strings
-        while True:
-            if pt == len(chars):  # if at the end
-                counter.append(curr_char)
-                counter.extend(list(str(count))) if count > 1 else None
-                break
-            if chars[pt] == curr_char:
-                count += 1  # increment counter if curr is the same char
-            else:
-                # append char and count (only if not 1)
-                counter.append(curr_char)
-                counter.extend(list(str(count))) if count > 1 else None
-                curr_char = chars[pt]  # set curr char to new char
-                count = 1  # reset count
-            pt += 1  # move pointer forward
-        chars[:] = counter  # modify input chars
-        return len(chars)
     
-    def compress_two_pointers(self, chars: List[str]) -> int:
+    def compress(self, chars: List[str]) -> int:
         """
             Time Complexity: O(n)
             Space Complexity: O(1)
@@ -53,11 +29,11 @@ class Solution:
             count = 0  # reset count
             write += 1  # move WRITE one char forward
             if read < len(chars):
-                # write where READ is at to current WRITE position
+                # write where READ is (next char) to the current WRITE position
                 chars[write] = chars[read]
         return write
     
-    def compress_two_pointers_2(self, chars: List[str]) -> int:
+    def compress_2(self, chars: List[str]) -> int:
         """
             Time Complexity: O(n)
             Space Complexity: O(1)
@@ -82,25 +58,16 @@ def main():
             line = input()
             chars1 = stringToList(line)
             chars2 = stringToList(line)
-            chars3 = stringToList(line)
             
             sol = Solution()
-            ret = sol.compress(chars1)
-            ret_pt = sol.compress_two_pointers(chars2)
-            ret_pt2 = sol.compress_two_pointers_2(chars3)
-            
-            out = str(ret)
-            out_pt = str(ret_pt)
-            out_pt2 = str(ret_pt2)
-            
-            print("Solved using O(n) counter:")
-            print(f"Compressed string: {chars1} of length {out}")
-            print()
+            ret1 = sol.compress(chars1)
+            ret2 = sol.compress_2(chars2)
+
             print("Solved using read and write pointers:")
-            print(f"Compressed string: {chars2} of length {out_pt}")
+            print(f"Compressed string: {chars1} of length {ret1}")
             print()
-            print("Solved using read and write pointers (second method:")
-            print(f"Compressed string: {chars3} of length {out_pt2}")
+            print("Solved using read and write pointers with anchor:")
+            print(f"Compressed string: {chars2} of length {ret2}")
         except StopIteration:
             break
 
