9f69907e2949c7daa583bf2ae188e3bb618063a3
Yingxuan Guo <yingxuan.guo9771@gmail.com>
2026-05-04 00:53:35 -0700

    LC_189 Add optimized rotate_reverse method and remove rotate_insert method

--- LC_189/Rotate Array.py
+++ LC_189/Rotate Array.py
@@ -7,38 +7,49 @@ from common_funcs import stringToList, listToString
 
 
 class Solution:
-    def rotate_insert(self, nums: List[int], k: int) -> None:
-        """
-            Time Complexity: O(n*k)
-            Space Complexity: O(1)
-        """
-        for i in range(k):
-            nums.insert(0, nums[-1])
-            nums.pop()
-        return
     
     def rotate_brute_force(self, nums: List[int], k: int) -> None:
         """
-            Time Complexity: O(n*k)
-            Space Complexity: O(1)
+        Manually move each element k steps to the right.
+        Time Complexity: O(n*k)
+        Space Complexity: O(1)
         """
         for i in range(k):
             prev = nums[-1]
             for j in range(len(nums)):
                 nums[j], prev = prev, nums[j]
-        return
     
     def rotate_extra_array(self, nums: List[int], k: int) -> None:
         """
-            Time Complexity: O(n)
-            Space Complexity: O(n)
+        Time Complexity: O(n)
+        Space Complexity: O(n)
         """
         n = len(nums)
         temp = [0] * n
         for i in range(n):
             temp[(i+k) % n] = nums[i]
         nums[:] = temp
-        return
+
+    def rotate_reverse(self, nums: List[int], k: int) -> None:
+        """
+        1. Reverse the entire array
+        2. Then reverse the first k elements
+        3. Finally, reverse the remaining n-k elements
+        
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        """
+        def reverse(nums: List[int], start: int, end: int) -> None:
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+
+        n = len(nums)
+        k %= n
+        reverse(nums, 0, n-1)
+        reverse(nums, 0, k-1)
+        reverse(nums, k, n-1)
 
 
 def main():
@@ -52,16 +63,16 @@ def main():
             k = int(line)
             
             sol = Solution()
-            sol.rotate_insert(nums1, k)
-            sol.rotate_brute_force(nums2, k)
-            sol.rotate_extra_array(nums3, k)
+            sol.rotate_brute_force(nums1, k)
+            sol.rotate_extra_array(nums2, k)
+            sol.rotate_reverse(nums3, k)
             
-            out_insert = listToString(nums1)
-            out_brute_force = listToString(nums2)
-            out_extra_array = listToString(nums3)
-            print(f"Solved using .insert():  {out_insert}")
+            out_brute_force = listToString(nums1)
+            out_extra_array = listToString(nums2)
+            out_reverse = listToString(nums3)
             print(f"Solved with brute force: {out_brute_force}")
             print(f"Solved with extra array: {out_extra_array}")
+            print(f"Solved with reverse:     {out_reverse}")
         except StopIteration:
             break
 
