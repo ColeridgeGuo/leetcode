163ad24180332a147bfa06c60523615f8f998adb
Yingxuan Guo <yingxuan.guo9771@gmail.com>
2026-09-10 19:29:35 -0700

    LC_79 modified solution

--- LC_79/Word Search.py
+++ LC_79/Word Search.py
@@ -12,34 +12,29 @@ from common_funcs import stringToList, stringToString
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         
-        def dfs(board, word, row, col, x):
-            """
-            row: the current row
-            col: the current column
-            x: the current index of letter to be matched in word
-            """
-            if x == len(word):  # if current index exceeds word length
+        def dfs(row: int, col: int, index: int) -> bool:
+            if index == len(word):  # if current index exceeds word length
                 return True
             if row < 0 or col < 0 or row >= len(board) or col >= len(board[0]):
                 # if current row/col exceeds the board boundaries
                 return False
-            if board[row][col] != word[x]:
+            if board[row][col] != word[index]:
                 # if the letter does not match the current letter in word
                 return False
             char = board[row][col]  # first char found, check remaining part
             board[row][col] = " "  # avoid re-visits
             # check all 4 neighbors with the rest of the word
-            res = dfs(board, word, row+1, col, x+1) or \
-                  dfs(board, word, row, col+1, x+1) or \
-                  dfs(board, word, row-1, col, x+1) or \
-                  dfs(board, word, row, col-1, x+1)
+            found = (dfs(row + 1, col, index + 1) or
+                     dfs(row, col + 1, index + 1) or
+                     dfs(row - 1, col, index + 1) or
+                     dfs(row, col - 1, index + 1))
             board[row][col] = char  # restore the char
-            return res
+            return found
         
         # find word starting from all positions
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if dfs(board, word, i, j, 0):
+                if dfs(i, j, 0):
                     return True
         return False
 
